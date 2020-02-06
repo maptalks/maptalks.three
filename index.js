9c5ff2249a43acbed24733260b1fdc51635cbcf0
fuzhenn <fuzhen@maptalks.org>
2020-02-06 12:07:59 +0800

    fix multiple renderScene

--- index.js
+++ index.js
@@ -730,7 +730,7 @@ class ThreeRenderer extends maptalks.renderer.CanvasLayerRenderer {
 
     _drawLayer() {
         super._drawLayer.apply(this, arguments);
-        this.renderScene();
+        // this.renderScene();
     }
 
     hitDetect() {
