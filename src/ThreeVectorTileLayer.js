bbe2f1b0a2a2edac4cf39721dd08281c314ed7ac
Iverson <mehudeyi@163.com>
2020-02-23 14:02:06 +0800

    add Terrain,TerrainVectorTileLayer

--- src/ThreeVectorTileLayer.js
+++ src/ThreeVectorTileLayer.js
@@ -2,26 +2,7 @@ import * as maptalks from 'maptalks';
 import * as THREE from 'three';
 import { pushQueue, outQueue, getQueues, nextLoop } from './queue/TileDataQueue';
 import { isGeoJSONPolygon, isGeoJSONLine, spliteGeoJSONMulti, isGeoJSONPoint, getGeoJSONCoordinates } from './util/GeoJSONUtil';
-
-const canvas = document.createElement('canvas');
-const SIZE = 256;
-canvas.width = canvas.height = SIZE;
-function generateImage(key, debug) {
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, SIZE, SIZE);
-    ctx.save();
-    if (debug) {
-        ctx.fillStyle = 'red';
-        ctx.strokeStyle = 'rgba(255,0,0,0.4)';
-        ctx.lineWidth = 0.2;
-        const text = key || 'tile';
-        ctx.font = '18px sans-serif';
-        ctx.rect(0, 0, SIZE, SIZE);
-        ctx.stroke();
-        ctx.fillText(text, 15, SIZE / 2);
-    }
-    return canvas.toDataURL();
-}
+import BaseVectorTileLayer from './BaseVectorTileLayer';
 
 const OPTIONS = {
     worker: false
@@ -29,7 +10,7 @@ const OPTIONS = {
 /**
  *Provide a simple data loading layer with large amount of data
  */
-class ThreeVectorTileLayer extends maptalks.TileLayer {
+class ThreeVectorTileLayer extends BaseVectorTileLayer {
     constructor(url, options = {}, getMaterial, layer) {
         super(maptalks.Util.GUID(), maptalks.Util.extend({ urlTemplate: url }, OPTIONS, options));
         this._opts = options;
@@ -40,37 +21,6 @@ class ThreeVectorTileLayer extends maptalks.TileLayer {
         this._add = null;
         this._layerLaodTime = new Date().getTime();
         this._init();
-    }
-
-    isAsynchronous() {
-        return this._opts.worker;
-    }
-
-    /**
-     *get current all baseobject
-     */
-    getBaseObjects() {
-        const loadTiles = this._loadTiles;
-        const baseos = [];
-        for (let key in loadTiles) {
-            const baseobjects = this._baseObjectKeys[key];
-            if (baseobjects && Array.isArray(baseobjects) && baseobjects.length) {
-                for (let i = 0, len = baseobjects.length; i < len; i++) {
-                    baseos.push(baseobjects[i]);
-                }
-            }
-        }
-        return baseos;
-    }
-
-    /**
-     * This method should be overridden for event handling
-     * @param {*} type
-     * @param {*} e
-     */
-    // eslint-disable-next-line no-unused-vars
-    onSelectMesh(type, e) {
-
     }
 
     /**
@@ -141,93 +91,6 @@ class ThreeVectorTileLayer extends maptalks.TileLayer {
         if (currentQueue.length > 0) {
             this.getTileData(q);
         }
-    }
-
-
-    /**
-     *
-     * @param {*} q
-     */
-    getTileData(q) {
-        const { key, url, callback, img } = q;
-        maptalks.Ajax.getJSON(url, {}, function (error, res) {
-            if (error) {
-                console.error(error);
-                callback(key, null, img);
-            } else {
-                callback(key, res, img);
-            }
-        });
-    }
-
-    _getCurentTileKeys() {
-        const tileGrids = this.getTiles().tileGrids || [];
-        const keys = [], keysMap = {};
-        for (let i = 0, len = tileGrids.length; i < len; i++) {
-            const d = tileGrids[i];
-            const tiles = d.tiles || [];
-            for (let j = 0, len1 = tiles.length; j < len1; j++) {
-                const { dupKey } = tiles[j];
-                keys.push(dupKey);
-                keysMap[dupKey] = true;
-            }
-
-        }
-        return { keys, keysMap };
-    }
-
-
-    _isLoad() {
-        const { keys } = this._getCurentTileKeys();
-        const keys1 = Object.keys(this._renderer.tilesInView);
-        if (keys.length === keys1.length) {
-            return true;
-        }
-        return false;
-    }
-
-
-    _layerOnLoad() {
-        // This event will be triggered multiple times per unit time
-        const time = new Date().getTime();
-        const offsetTime = time - this._layerLaodTime;
-        if (offsetTime < 20) {
-            return;
-        }
-        this._layerLaodTime = time;
-        const tilesInView = this._renderer.tilesInView, loadTiles = this._loadTiles, threeLayer = this._layer, keys = this._baseObjectKeys;
-        const tilesInViewLen = Object.keys(tilesInView).length, loadTilesLen = Object.keys(loadTiles).length;
-        const needsRemoveBaseObjects = [];
-        if (tilesInViewLen && loadTilesLen) {
-            for (let index in loadTiles) {
-                if (!tilesInView[index]) {
-                    if (keys[index]) {
-                        (keys[index] || []).forEach(baseobject => {
-                            needsRemoveBaseObjects.push(baseobject);
-                        });
-                    }
-                }
-            }
-        }
-        if (needsRemoveBaseObjects.length) {
-            threeLayer.removeMesh(needsRemoveBaseObjects, false);
-        }
-        if (tilesInViewLen && loadTilesLen) {
-            for (let index in tilesInView) {
-                if (!loadTiles[index]) {
-                    if (keys[index]) {
-                        const baseobject = keys[index];
-                        threeLayer.addMesh(baseobject);
-                    } else {
-                        const splitstr = index.indexOf('_') > -1 ? '_' : '-';
-                        const [y, x, z] = index.split(splitstr).slice(1, 4);
-                        this.getTileUrl(x, y, z);
-                    }
-                }
-            }
-        }
-        this._loadTiles = Object.assign({}, tilesInView);
-        this._diffCache();
     }
 
     _init() {
@@ -318,119 +181,6 @@ class ThreeVectorTileLayer extends maptalks.TileLayer {
         return null;
     }
 
-    _workerLoad(e) {
-        const baseobject = e.target;
-        const img = baseobject._img;
-        img.currentCount++;
-        if (img.currentCount === img.needCount) {
-            img.src = generateImage(img._key, this._opts.debug);
-        }
-    }
-
-
-    _generateBaseObjects(index, json, img) {
-        if (json && img) {
-            const { keysMap } = this._getCurentTileKeys();
-            //not in current ,ignore
-            if (!keysMap[index]) {
-                img.src = generateImage(index, this._opts.debug);
-                return;
-            }
-            const baseobjects = this.formatBaseObjects(index, json);
-            if (baseobjects.length) {
-                img.needCount = baseobjects.length;
-                img.currentCount = 0;
-                for (let i = 0, len = baseobjects.length; i < len; i++) {
-                    const baseobject = baseobjects[i];
-                    baseobject._img = img;
-                    baseobject._vt = this;
-                    if (!this.isVisible()) {
-                        baseobject.hide();
-                    }
-                    this._cachetile(index, baseobject);
-                    if (!baseobject.isAsynchronous()) {
-                        img.currentCount++;
-                    }
-                }
-
-                this._layer.addMesh(baseobjects, false);
-                if (img.needCount === img.currentCount) {
-                    img.src = generateImage(index, this._opts.debug);
-                }
-                if (this.isAsynchronous()) {
-                    baseobjects.filter(baseobject => {
-                        return baseobject.isAsynchronous();
-                    }).forEach(baseobject => {
-                        baseobject.on('workerload', this._workerLoad, this);
-                    });
-                } else {
-                    img.src = generateImage(index, this._opts.debug);
-                }
-            } else {
-                img.src = generateImage(index, this._opts.debug);
-            }
-            this._loadTiles[index] = true;
-        } else if (img) {
-            img.src = generateImage(index, this._opts.debug);
-        }
-    }
-
-    _diffCache() {
-        // if (this._layer.getMap().isInteracting()) {
-        //     return;
-        // }
-        if (Object.keys(this._baseObjectKeys).length > this._renderer.tileCache.max) {
-            const tileCache = this._renderer.tileCache.data;
-            const tilesInView = this._renderer.tilesInView;
-            const needsRemoveBaseObjects = [];
-            for (let index in this._baseObjectKeys) {
-                if (!tileCache[index] && !tilesInView[index]) {
-                    (this._baseObjectKeys[index] || []).forEach(baseobject => {
-                        if (baseobject.isAdd) {
-                            needsRemoveBaseObjects.push(baseobject);
-                        }
-                    });
-                    this._diposeBaseObject(index);
-                    delete this._baseObjectKeys[index];
-                }
-            }
-            // Batch deletion can have better performance
-            if (needsRemoveBaseObjects.length) {
-                this._layer.removeMesh(needsRemoveBaseObjects, false);
-            }
-        }
-    }
-
-    _diposeBaseObject(index) {
-        const baseobjects = this._baseObjectKeys[index];
-        if (baseobjects && baseobjects.length) {
-            baseobjects.forEach(baseobject => {
-                baseobject.getObject3d().geometry.dispose();
-                if (baseobject._geometryCache) {
-                    baseobject._geometryCache.dispose();
-                }
-                const bos = baseobject._baseObjects;
-                if (bos && bos.length) {
-                    bos.forEach(bo => {
-                        bo.getObject3d().geometry.dispose();
-                        bo = null;
-                    });
-                }
-                baseobject._datas = null;
-                baseobject._geometriesAttributes = null;
-                baseobject._faceMap = null;
-                baseobject = null;
-            });
-        }
-    }
-
-    _cachetile(index, baseobject) {
-        if (!this._baseObjectKeys[index]) {
-            this._baseObjectKeys[index] = [];
-        }
-        this._baseObjectKeys[index].push(baseobject);
-
-    }
 }
 
 export default ThreeVectorTileLayer;
---
+++ src/util/CanvasUtil.js
@@ -0,0 +1,22 @@
+
+const canvas = document.createElement('canvas');
+const SIZE = 256;
+canvas.width = canvas.height = SIZE;
+
+
+export function generateImage(key, debug) {
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, SIZE, SIZE);
+    ctx.save();
+    if (debug) {
+        ctx.fillStyle = 'red';
+        ctx.strokeStyle = 'rgba(255,0,0,0.4)';
+        ctx.lineWidth = 0.2;
+        const text = key || 'tile';
+        ctx.font = '18px sans-serif';
+        ctx.rect(0, 0, SIZE, SIZE);
+        ctx.stroke();
+        ctx.fillText(text, 15, SIZE / 2);
+    }
+    return canvas.toDataURL();
+}
