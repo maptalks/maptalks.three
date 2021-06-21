8cc0c1fa9bbd615f444b3a83dfaace4d406ec323
deyihu <mehudeyi@163.com>
2021-06-21 09:52:43 +0800

    refactory fatline util

--- src/util/fatline/Line2.ts
+++ src/util/fatline/Line2.ts
@@ -7,31 +7,20 @@ import LineSegments2 from './LineSegments2';
 import LineGeometry from './LineGeometry';
 import LineMaterial from './LineMaterial';
 
-const Line2 = function (geometry, material) {
+class Line2 extends LineSegments2 {
+    isLine2: boolean = true;
+    _colorIndex: number;
+    constructor(geometry, material) {
+        super(geometry, material);
+        this.type = 'Line2';
 
-    LineSegments2.call(this);
-
-    this.type = 'Line2';
-
-    this.geometry = geometry !== undefined ? geometry : new LineGeometry();
-    this.material = material !== undefined ? material : new LineMaterial({ color: Math.random() * 0xffffff });
-
-};
-
-Line2.prototype = Object.assign(Object.create(LineSegments2.prototype), {
-
-    constructor: Line2,
-
-    isLine2: true,
-
-    // eslint-disable-next-line no-unused-vars
-    copy: function (source) {
-
-        // todo
-
-        return this;
-
+        this.geometry = geometry !== undefined ? geometry : new LineGeometry();
+        this.material = material !== undefined ? material : new LineMaterial({ color: Math.random() * 0xffffff });
     }
 
-});
+    copy(source) {
+
+        return this;
+    }
+};
 export default Line2;
